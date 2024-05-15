8ab0265efee0ff0841843a42cea5565d23bd94ec
JCesar25 <cruzlino021@gmail.com>
2024-05-14 23:18:52 -0400

    2daversionTiTlee

--- src/Componentes/Hero.js
+++ src/Componentes/Hero.js
@@ -1,28 +1,48 @@
-import React from 'react';
-import heroImage from "../Assest/Images/lg.jpg"
+import React from "react";
+import heroImage from "../Assest/Images/lg.jpg";
+import { FaWhatsapp } from 'react-icons/fa';
 
 const Hero = () => (
   <section className="hero-section position-relative text-center text-white">
-    <img src={heroImage} alt="Imagen principal" className="img-fluid w-100" style={styles.image} />
-    <div className="position-absolute top-50 start-50 translate-middle" style={styles.text}>
+    <img
+      src={heroImage}
+      alt="Imagen principal"
+      className="img-fluid mt-4"
+      style={styles.image}
+    />
+    <div
+      className="position-absolute top-50 start-50 translate-middle"
+      style={styles.text}
+    >
       <h1 className="display-4">PÉRGOLAS A MEDIDA</h1>
-      <p className="lead">¿Quiere protegerse del sol con nuestras pérgolas personalizadas?</p>
+      <p className="lead">
+        ¿Quiere protegerse del sol con nuestras pérgolas personalizadas?
+      </p>
+      <div className="mt-4">
+        <a
+          href="https://wa.me/59169168685?text=Tengo%20una%20consulta%20con%20las%20pérgolas"
+          className="btn btn-success"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaWhatsapp /> Contáctenos en WhatsApp
+        </a>
+      </div>
     </div>
   </section>
 );
 
 const styles = {
   image: {
-    height: '70vh',
-    objectFit: 'cover',
+    height: "70vh",
+    objectFit: "cover",
   },
   text: {
-    color: '#fff',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    padding: '20px',
-    borderRadius: '10px',
+    color: "#fff",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    padding: "20px",
+    borderRadius: "10px",
   },
 };
 
 export default Hero;
-
